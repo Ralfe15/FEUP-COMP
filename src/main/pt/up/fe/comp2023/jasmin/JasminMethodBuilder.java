6ac9a93f5b44eb9a05859968b7f6e5b991915ded
Guilherme Moreira <gui29102002@gmail.com>
2023-05-27 20:56:05 +0000

    reverted stack and local limits

--- src/main/pt/up/fe/comp2023/jasmin/JasminMethodBuilder.java
+++ src/main/pt/up/fe/comp2023/jasmin/JasminMethodBuilder.java
@@ -41,11 +41,8 @@ public class JasminMethodBuilder {
             }
         }
 
-        methodDefinition.append("\t.limit stack 99\n");
-        methodDefinition.append("\t.limit locals 99\n");
-
-        //methodDefinition.append("\t.limit stack ").append(instructionGenerator.getMaxLoadCounter()).append("\n");
-        //methodDefinition.append("\t.limit locals ").append(this.getLocalsLimit()).append("\n");
+        methodDefinition.append("\t.limit stack ").append(instructionGenerator.getMaxLoadCounter()).append("\n");
+        methodDefinition.append("\t.limit locals ").append(this.getLocalsLimit()).append("\n");
 
         methodDefinition.append(instructions);
 
